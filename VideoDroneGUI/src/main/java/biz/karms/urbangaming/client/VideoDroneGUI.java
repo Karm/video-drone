7ce44539e40f0174df2223e523943b2339448546
Michal Karm Babacek <karm@email.cz>
2012-02-19 16:35:00 +0100

    Initial GWT GUI import. mvn gwt:run  :-)

---
+++ VideoDroneGUI/src/main/java/biz/karms/urbangaming/client/VideoDroneGUI.java
@@ -0,0 +1,28 @@
+package biz.karms.urbangaming.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.DecoratorPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.ResizeLayoutPanel;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * 
+ * @author Michal Karm Babacek
+ *
+ */
+public class VideoDroneGUI implements EntryPoint {
+	private final DecoratorPanel decorator = new DecoratorPanel();
+	
+	public void onModuleLoad() {
+		
+		ResizeLayoutPanel resizePanel = new ResizeLayoutPanel();
+		resizePanel.setPixelSize(600, 400);
+		resizePanel.setWidget(new HTML("meh..."));
+		
+		decorator.add(resizePanel);
+		RootPanel.get("perunWeb").add(decorator);
+
+	}
+	
+}
