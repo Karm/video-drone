adf34103df5a6051af5a5b46fe5845ba93e7e30f
Michal Karm Babacek <karm@email.cz>
2012-02-20 02:26:46 +0100

    Google Web Toolkit (GWT) + WebSockets works with VideoDroneServer. Whhheeeehaaaaa\!

--- VideoDroneGUI/src/main/java/biz/karms/urbangaming/client/VideoDroneGUI.java
+++ VideoDroneGUI/src/main/java/biz/karms/urbangaming/client/VideoDroneGUI.java
@@ -1,5 +1,8 @@
 package biz.karms.urbangaming.client;
 
+import biz.karms.urbangaming.client.service.WebSocket;
+import biz.karms.urbangaming.client.service.WebSocketCallback;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.DecoratorPanel;
 import com.google.gwt.user.client.ui.HTML;
@@ -9,20 +12,35 @@ import com.google.gwt.user.client.ui.RootPanel;
 /**
  * 
  * @author Michal Karm Babacek
- *
+ * 
  */
 public class VideoDroneGUI implements EntryPoint {
-	private final DecoratorPanel decorator = new DecoratorPanel();
-	
-	public void onModuleLoad() {
-		
-		ResizeLayoutPanel resizePanel = new ResizeLayoutPanel();
-		resizePanel.setPixelSize(600, 400);
-		resizePanel.setWidget(new HTML("meh..."));
-		
-		decorator.add(resizePanel);
-		RootPanel.get("videoDroneGUI").add(decorator);
+    private final DecoratorPanel decorator = new DecoratorPanel();
+    private final ResizeLayoutPanel resizePanel = new ResizeLayoutPanel();
+    private final String server = "ws://localhost:8080/";
+    
+    public void onModuleLoad() {
+        resizePanel.setPixelSize(600, 400);
+        resizePanel.setWidget(new HTML("meh..."));
+        decorator.add(resizePanel);
+        RootPanel.get("videoDroneGUI").add(decorator);
+        WebSocketCallback webSocketCallback = new WebSocketCallback() {
+            
+            public void onMessage(String message) {
+                resizePanel.setWidget(new HTML("Message received: "+message));
+            }
+            
+            public void onDisconnect() {
+                resizePanel.setWidget(new HTML("Disconnected."));
+            }
+            
+            public void onConnect() {
+                resizePanel.setWidget(new HTML("Connected."));
+            }
+        };
+        
+        WebSocket webSocket = new WebSocket(webSocketCallback);
+        webSocket.connect(server);
+    }
 
-	}
-	
 }
